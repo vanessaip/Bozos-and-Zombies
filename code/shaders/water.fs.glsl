34a0dfdb457d609d8d10f1c147bb40144316166c
DESKTOP-SL91OII\Justin <justin.koerner95@gmail.com>
2023-12-05 17:04:44 -0800

    Fix game balance by removing some zombies

--- code/shaders/water.fs.glsl
+++ code/shaders/water.fs.glsl
@@ -14,12 +14,12 @@ vec2 distort(vec2 uv)
 	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	// TODO A1: HANDLE THE WATER WAVE DISTORTION HERE (you may want to try sin/cos)
 	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-if (!is_poisoned){
-	return uv;
-}
-	float distortionStrength = 0.005; 
-    vec2 distortion = vec2(sin(time/2) * distortionStrength, cos(time/2) * distortionStrength);
-    return uv + distortion;
+	if (!is_poisoned){
+		return uv;
+	}
+		float distortionStrength = 0.005; 
+		vec2 distortion = vec2(sin(time/2) * distortionStrength, cos(time/2) * distortionStrength);
+		return uv + distortion;
 }
 
 vec4 color_shift(vec4 in_color) 
@@ -27,13 +27,13 @@ vec4 color_shift(vec4 in_color)
 	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	// TODO A1: HANDLE THE COLOR SHIFTING HERE (you may want to make it blue-ish)
 	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-if (!is_poisoned){
-	return in_color;
-}
-	vec4 purpleTint = vec4(0.5, 0.2, 0.5, 1.0);
-    float mixFactor = 0.5;
-    return mix(in_color, purpleTint, mixFactor);
-}
+	if (!is_poisoned){
+		return in_color;
+	}
+		vec4 purpleTint = vec4(0.5, 0.2, 0.5, 1.0);
+		float mixFactor = 0.5;
+		return mix(in_color, purpleTint, mixFactor);
+	}
 
 vec4 fade_color(vec4 in_color) 
 {
